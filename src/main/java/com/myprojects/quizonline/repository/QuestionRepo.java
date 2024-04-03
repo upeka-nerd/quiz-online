027b66ef696daed4f1a5b44ad2b1aec9949adf1a
Pasindu Upeka <upekasilva2000@gmail.com>
2024-04-03 10:18:52 +0530

    done main crud

--- src/main/java/com/myprojects/quizonline/repository/QuestionRepo.java
+++ src/main/java/com/myprojects/quizonline/repository/QuestionRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +15,7 @@ import java.util.List;
 @EnableJpaRepositories
 public interface QuestionRepo extends JpaRepository<Question,Long> {
 
-
+    @Query("SELECT DISTINCT q.subject FROM Question q ")
     List<String> findAllDistinctSubject();
-
     Page<Question> findBySubject(String subject, Pageable pageable);
 }
